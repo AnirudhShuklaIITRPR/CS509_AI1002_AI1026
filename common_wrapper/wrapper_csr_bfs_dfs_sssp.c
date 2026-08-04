d50280deaf978644ca897eda26233e27ec3a31b6
AnirudhShuklaIITRPR <2026aim1002@iitrpr.ac.in>
2026-08-04 22:38:50 +0530

    Implement graph algorithms using CSR representation
    This file implements a console application for graph algorithms, including BFS and DFS, using CSR representation. It reads a graph from a file, allows the user to choose an algorithm, and outputs execution time.

---
+++ common_wrapper/wrapper_csr_bfs_dfs_sssp.c
@@ -0,0 +1,112 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "C:\Users\Dell\Downloads\CS509\Assignment_1_buddy\include\graph.h"
+#include "C:\Users\Dell\Downloads\CS509\Assignment_1_buddy\include\csr.h"
+#include "C:\Users\Dell\Downloads\CS509\Assignment_1_buddy\include\bfs.h"
+#include "C:\Users\Dell\Downloads\CS509\Assignment_1_buddy\include\dfs.h"
+
+int main()
+{
+    Graph graph;
+    CSR csr;
+
+    int choice;
+    int source;
+
+    clock_t start, end;
+    double executionTime;
+
+    // Read Graph
+    readGraph(&graph, "test/test_case_5.txt");
+
+    // Convert Adjacency List to CSR
+
+    csr = convertToCSR(graph);
+    FILE *fo = fopen("output/output5.txt", "w");
+
+    if (fo == NULL)
+    {
+        printf("Unable to create output file.\n");
+        return 1;
+    }
+
+    do
+    {
+        printf("\n=====================================\n");
+        printf("      GRAPH ALGORITHMS MENU\n");
+        printf("=====================================\n");
+        printf("1. Breadth First Search (BFS)\n");
+        printf("2. Depth First Search (DFS)\n");
+        printf("3. Print CSR Representation\n");
+        printf("4. Exit\n");
+        printf("=====================================\n");
+
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch(choice)
+        {
+            case 1:
+
+                printf("Enter Source Vertex: ");
+                scanf("%d", &source);
+
+                start = clock();
+
+                BFS(csr, source);
+
+                end = clock();
+
+                executionTime = (double)(end-start)/CLOCKS_PER_SEC;
+
+                printf("\nExecution Time : %.6f seconds\n",executionTime);
+                fprintf(fo,"\nBFS Source : %d\n",source);
+                fprintf(fo,"Execution Time : %.6f seconds\n",executionTime);
+
+                break;
+
+            case 2:
+
+                printf("Enter Source Vertex: ");
+                scanf("%d",&source);
+
+                start = clock();
+
+                DFS(csr,source);
+
+                end = clock();
+
+                executionTime = (double)(end-start)/CLOCKS_PER_SEC;
+
+                printf("\nExecution Time : %.6f seconds\n", executionTime);
+
+                fprintf(fo,"\nDFS Source : %d\n",source);
+                fprintf(fo,"Execution Time : %.6f seconds\n", executionTime);
+
+                break;
+
+            case 3:
+
+                printCSR(csr);
+
+                break;
+
+            case 4:
+
+                printf("\nProgram Terminated.\n");
+
+                break;
+
+            default:
+
+                printf("\nInvalid Choice.\n");
+        }
+
+    }while(choice != 4);
+    fclose(fo);
+    freeCSR(&csr);
+    freeGraph(&graph);
+    return 0;
+}
