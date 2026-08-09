d6eca634117d58ac46de7efe324917e74d1bd70d
AnirudhShuklaIITRPR <2026aim1002@iitrpr.ac.in>
2026-08-09 23:22:22 +0530

    Refactor driver.c for improved functionality and structure

--- Assignment_2/driver/driver.c
+++ Assignment_2/driver/driver.c
@@ -2,66 +2,106 @@
 #include <stdlib.h>
 #include <time.h>
 
-#include "csr.h"
-#include "connected_components.h"
+#include "C:\Users\Dell\Downloads\CS509\BUDDY\Assignment_2\include\csr.h"
+#include "C:\Users\Dell\Downloads\CS509\BUDDY\Assignment_2\include\connected_components.h"
+#include "C:\Users\Dell\Downloads\CS509\BUDDY\Assignment_2\include\triangle_counting.h"
+#include "C:\Users\Dell\Downloads\CS509\BUDDY\Assignment_2\include\betweeness_centrality.h"
 
 int main()
 {
     int choice;
-    char filename[100];
+    int testCase;
 
-    while (1)
-    {
-        printf("\n===== ASSIGNMENT 2 =====\n");
+    char filename[150];
+    char outputFile[200];
+
+    while (1){
+        
+        printf("\nCS509 ASSIGNMENT 2\n");
         printf("1. Connected Components\n");
         printf("2. Triangle Counting\n");
         printf("3. Betweenness Centrality\n");
         printf("4. Exit\n");
+        printf("-------------------------------------\n");
 
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        if (choice == 4)
-        {
-            printf("Exiting program...\n");
+        if (choice == 4){
+            printf("\nProgram terminated.\n");
             break;
         }
 
-        printf("Enter input filename: ");
-        scanf("%s", filename);
+        if (choice < 1 || choice > 4){
+            printf("\nInvalid choice.\n");
+            continue;
+        }
 
-        switch (choice)
+
+        printf("\nAvailable Test Cases:\n");
+        printf("10\n");
+        printf("100\n");
+        printf("1000\n");
+        printf("10000\n");   // Large ones are not for Betweeness Centrality
+        printf("50000\n");
+        printf("100000\n");
+
+        printf("\nEnter test case: ");
+        scanf("%d", &testCase);
+
+        sprintf(filename, "test/test_case_%d.txt", testCase);
+
+        if (choice == 1)
         {
-        case 1:
+            sprintf(outputFile, "output/connectedcomponents/output_%d.txt", testCase);
+        }
+        else if (choice == 2)
         {
-            // Connected Components
+            sprintf(outputFile, "output/trianglecounting/output_%d.txt", testCase);
+        }
+        else
+        {
+            sprintf(outputFile, "output/betweennesscentrality/output_%d.txt", testCase);
+        }
+
+        FILE *fo = fopen(outputFile, "w");
+
+        if (fo == NULL)
+        {
+            printf("\nUnable to create output file:\n");
+            printf("%s\n", outputFile);
+            printf("Make sure the output folders exist.\n");
+            continue;
+        }
+
+        if (choice == 1)
+        {
+            printf("\nCONNECTED COMPONENTS\n");
 
             readGraph(filename);
-
             convertCSR();
 
-            int *component =
-                (int *)malloc(vertices * sizeof(int));
+            int *component = (int *)malloc(vertices * sizeof(int));
+
+            if (component == NULL)            {
+                printf("Unable to allocate memory.\n");
+
+                fclose(fo);
+                freeGraph();
+
+                continue;
+            }
 
             clock_t start = clock();
-
-            int componentCount =
-                connectedComponents(
-                    vertices,
-                    row_ptr,
-                    col_idx,
-                    component);
-
+            int componentCount = connectedComponents(vertices, row_ptr, col_idx, component);
             clock_t end = clock();
-
-            double executionTime =
-                (double)(end - start) / CLOCKS_PER_SEC;
+            double executionTime = (double)(end - start) / CLOCKS_PER_SEC;
 
             printf("\nAlgorithm: Connected Components\n");
-            printf("Number of components: %d\n",
-                   componentCount);
+            printf("Test Case: %d\n", testCase);
+            printf("Number of components: %d\n", componentCount);
 
-            printf("Vertex Component\n");
+            printf("\nVertex Component\n");
 
             for (int i = 0; i < vertices; i++)
             {
@@ -70,31 +110,97 @@ int main()
                        component[i]);
             }
 
-            printf("Execution time: %.6f seconds\n",
-                   executionTime);
+            printf("\nExecution Time: %.12f seconds\n", executionTime);
+            
+            fprintf(fo, "CONNECTED COMPONENTS RESULT\n");
+            fprintf(fo, "Test Case: %d\n", testCase);
+            fprintf(fo, "Input File: %s\n\n", filename);
+            fprintf(fo, "Number of components: %d\n\n", componentCount);
+            fprintf(fo, "Vertex Component\n");
+            for (int i = 0; i < vertices; i++){
+                fprintf(fo, "%d %d\n", i, component[i]);
+            }
+
+            fprintf(fo, "\nExecution Time: %.12f seconds\n", executionTime);
 
             free(component);
+            freeGraph();
+        }
 
+        else if (choice == 2)
+        {
+            printf("\nTRIANGLE COUNTING\n");
+
+            readGraph(filename);
+            convertCSR();
+
+            clock_t start = clock();
+            int triangleCount = triangleCounting(vertices, row_ptr, col_idx);
+            clock_t end = clock();
+            double executionTime = (double)(end - start) / CLOCKS_PER_SEC; 
+
+            printf("\nAlgorithm: Triangle Counting\n");
+            printf("Test Case: %d\n", testCase);
+            printf("Number of triangles: %d\n", triangleCount);
+            printf("Execution Time: %.12f seconds\n", executionTime);
+
+            fprintf(fo, "TRIANGLE COUNTING RESULT\n");
+            fprintf(fo, "Test Case: %d\n", testCase);
+            fprintf(fo, "Input File: %s\n\n", filename);
+            fprintf(fo, "Number of triangles: %d\n", triangleCount);
+            fprintf(fo, "Execution Time: %.12f seconds\n", executionTime);
+
+            freeGraph();
+        }
+
+        else if (choice == 3)
+        {
+            printf("\nBETWEENNESS CENTRALITY\n");
+            
+            readGraph(filename);
+            convertCSR();
+            
+            double *centrality = (double *)malloc(vertices * sizeof(double));
+            
+            if (centrality == NULL){
+                printf("Unable to allocate memory.\n");
+                
+                fclose(fo);
+                freeGraph();
+                
+                continue;
+            }
+            
+            clock_t start = clock();
+            betweennessCentrality(vertices, row_ptr, col_idx, centrality);
+            clock_t end = clock();
+            double executionTime = ((double)(end - start) / CLOCKS_PER_SEC) * 1000.0;
+            
+            printf("\nAlgorithm: Betweenness Centrality\n");
+            printf("Test Case: %d\n\n", testCase);
+            printf("Vertex Centrality\n");
+            
+            for (int i = 0; i < vertices; i++){
+                printf("%d %.2f\n", i, centrality[i]);
+            }
+            
+            printf("\nExecution Time: %.12f ms\n", executionTime);
+            
+            fprintf(fo, "Algorithm: Betweenness Centrality\n");
+            fprintf(fo, "Test Case: %d\n\n", testCase);
+            fprintf(fo, "Vertex Centrality\n");
+            for (int i = 0; i < vertices; i++){
+                fprintf(fo, "%d %.2f\n", i, centrality[i]);
+            }
+            
+            fprintf(fo, "\nExecution Time: %.6f ms\n", executionTime);
+            
+            free(centrality);
             freeGraph();
 
             break;
         }
-
-        case 2:
-            // Triangle Counting
-            // Buddy Anirudh will implement
-            printf("Triangle Counting selected.\n");
-            break;
-
-        case 3:
-            // Betweenness Centrality
-            // Buddy Anirudh will implement
-            printf("Betweenness Centrality selected.\n");
-            break;
-
-        default:
-            printf("Invalid choice. Please try again.\n");
-        }
+        fclose(fo);
     }
 
     return 0;
