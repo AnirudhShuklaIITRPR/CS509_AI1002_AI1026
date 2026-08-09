510e3302a0160175a2094bbfc4c310f8f0fe1c59
AnirudhShuklaIITRPR <2026aim1002@iitrpr.ac.in>
2026-08-09 23:35:22 +0530

    Add files via upload

---
+++ Assignment_2/include/betweeness_centrality.h
@@ -0,0 +1,6 @@
+#ifndef BETWEENNESS_CENTRALITY_H
+#define BETWEENNESS_CENTRALITY_H
+
+void betweennessCentrality(int vertices, int *row_ptr, int *col_idx, double *centrality);
+
+#endif
---
+++ Assignment_2/include/triangle_counting.h
@@ -0,0 +1,6 @@
+#ifndef TRIANGLE_COUNTING_H
+#define TRIANGLE_COUNTING_H
+
+int triangleCounting(int vertices, int *row_ptr, int *col_idx);
+
+#endif
